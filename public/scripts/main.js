bc94bae34b2e1292adb7708337583a9b6c7ceccd
Deividadrian <73372786+Deividadrian@users.noreply.github.com>
2021-12-27 20:42:17 -0300

    Add Controllersn
    Middleware configured in added routes and controllers

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,20 +11,30 @@ const checkButtons = document.querySelectorAll('.actions a.check')
 
 checkButtons.forEach(button => {
   //Adicinar a escuta
-  button.addEventListener('click', event => {
-    modalTitle.innerHTML = 'Marcar como lida'
-    //open modal
-    modal.open()
-  })
+  button.addEventListener('click', handleClick)
 })
 
 //When the delete button is clicked, the modal opens
 const deleteButton = document.querySelectorAll('.actions a.delete')
 
 deleteButton.forEach(button => {
-  button.addEventListener('click', event => {
-    modalTitle.innerHTML = 'Excluir essa pergunta'
-    //open modal
-    modal.open()
-  })
+  button.addEventListener('click', event => handleClick(event, false))
 })
+
+function handleClick(event, check = true) {
+  event.preventDefault() //Evita a acrecimo de # ou outros caracteres na URL quando e clicado no Marcar como lido ou em Ecluir.
+  const text = check ? 'Marcar como lido ' : 'Excluir '
+  const roomId = document.querySelector('#room-id').dataset.id
+  const slug = check ? 'check' : 'delete'
+  const questionId = event.target.dataset.id // Pega o elemento que acontece no evento data-id
+
+  const form = document.querySelector('.modal form')
+  form.setAttribute('action', `/room/${roomId}/${questionId}/${slug}`) // Informaçoes para o formulasrio
+
+  modalTitle.innerHTML = `${text} essa pergunta `
+  modalDescription.innerHTML = `Tem certeza que você deseja ${text.toLowerCase()} esta pergunta?`
+  modalButton.innerHTML = `Sim, ${text.toLowerCase()}`
+  check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+  //open modal
+  modal.open()
+}
--- src/route.js
+++ src/route.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const QuestionControler = require('./controllers/QuestionController')
 
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index'))
 route.get('/room', (req, res) => res.render('room'))
 route.get('/create-pass', (req, res) => res.render('create-pass'))
+
+//Formato que o formulario de dentro da modal tem que passa a informação
+route.post('/room/:room/:question/:action', QuestionControler.index)
 
 module.exports = route
